3f9f7f69a44001855f37aa3de0777189d60fd2c1
purk99 <66315113+purk99@users.noreply.github.com>
2022-05-23 09:05:56 +0200

    added toolsv2.1

--- TestWindow.py
+++ TestWindow.py
@@ -1,8 +1,18 @@
 from math import ceil
+from pickletools import uint8
 import string
-from numpy import char, float16, int16
+from numpy import char, float16, int16, uint16
 import pigpio
 
+val1 = uint16(4135)
+val2 = uint16((val1 >> 8) | (val1 << 8))
+
+print("Value 1: ")
+print(val1,end='\n')
+print("Value 2: ")
+print(val2)
+
+'''
 ina226 = pigpio.pi()
 testlist = [0xff,0xff]
 ina226.i2c_write_i2c_block_data(0x40,0x5,testlist)
@@ -24,6 +34,7 @@ print(round(nvar1,1),end='\t')
 print(round(nvar2,2),end='\t')
 print(ceil(nvar2),end='\t')
 print(int16(nvar2))
+'''
 
 
 

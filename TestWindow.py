3f4ca28628491362432aab314d160b67aba49393
purk99 <66315113+purk99@users.noreply.github.com>
2022-06-03 15:31:02 +0200

    tested implementation of full eeprom

--- TestWindow.py
+++ TestWindow.py
@@ -3,39 +3,31 @@ import string
 from numpy import char, float16, int16, uint16, uint8
 import pigpio
 
-val1 = uint8(0x50)
-val2 = val1 >> 4
+from tkinter import ttk
+from tkinter import *
 
-print("Value 1: ")
-print(val1,end='\n')
-print("Value 2: ")
-print(val2)
-
-'''
-ina226 = pigpio.pi()
-testlist = [0xff,0xff]
-ina226.i2c_write_i2c_block_data(0x40,0x5,testlist)
-
-var1 = float16(10)
-var2 = float16(0.01)
-var3 = [0xff,0xff,0xff]
-
-nvar1 = float16(var1/(2**15))
-nvar2 = int16(0.00512/(nvar1*var2))
-
-print(len(var3),end='\t')
-
-print(var1,end='\t')
-print(var2,end='\t')
-print(nvar1,end='\t')
-print(nvar2,end='\t')
-print(round(nvar1,1),end='\t')
-print(round(nvar2,2),end='\t')
-print(ceil(nvar2),end='\t')
-print(int16(nvar2))
-'''
+fields = list(range(0,100))
 
 
+root = Tk()
+root.geometry("800x480")
+
+frame = ttk.Frame(root)
+frame.grid()
+
+counter = 0
+counter1 = 0
+
+for i in fields:
+    ttk.Separator(frame,orient=VERTICAL).grid(column=(counter*2)+1,row=counter1)
+    ttk.Label(frame, text=fields[i]).grid(column=counter*2,row=counter1)
+    #ttk.Label(frame,text=100-fields[i]).grid(column=(counter*2)+2,row=counter1)
+    counter1 += 1
+    if fields[i]%10 == 9:
+        counter += 1
+        counter1 = 0
+
+root.mainloop()
 
 
 
